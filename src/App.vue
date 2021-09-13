680cf999b76af14ca15761e661dd31a874f0469b
Evgeny Ukhanov <mrlsd@ya.ru>
2021-09-13 22:07:21 +0300

    Initial UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Thank you for using our product. We're glad you're with us."/>
 </template>
 
 <script>
@@ -16,11 +15,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
 }
 </style>
